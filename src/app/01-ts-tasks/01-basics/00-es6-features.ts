53d06a3810f39180702ae1cea4ffe789d68de0ea
Piotr Woloszun <piotr.woloszun@gmail.com>
2017-06-05 06:56:43 +0200

    es6 features

---
+++ src/app/01-ts-tasks/01-basics/00-es6-features.ts
@@ -0,0 +1,38 @@
+function es6Examples() {
+  // constants
+
+  // block scopes
+
+  // template literals
+
+  // property shorthand
+
+  // computed property names
+
+  // destructuring array
+
+  // destructuring object
+
+  // parameter context matching
+
+
+
+}
+
+// default param
+// rest param
+// spread operator
+function createPerson(name, age, sex) {
+
+}
+
+
+
+export function es6FeaturesApp() {
+
+}
+
+// modules import & export
+
+
+
